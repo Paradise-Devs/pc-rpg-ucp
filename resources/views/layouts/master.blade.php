4706ac129818117bf3480583e00b7649d269b107
Lucas Godoy <lcsdegodoy@outlook.com>
2016-06-21 17:30:41 -0300

    Remoção de comentários redundantes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,53 +5,34 @@
 </head>
 
 <body class="dashboard-page">
-    <!-- Start: Main -->
+
     <div id="main">
-        <!-- Start: Header -->
+
         @include('partials._header')
-        <!-- End: Header -->
-
-        <!-- Start: Sidebar -->
         @include('partials._sidebar')
-        <!-- End: Sidebar Left -->
 
-        <!-- Start: Content-Wrapper -->
         <section id="content_wrapper">
-            <!-- Start: Topbar-Dropdown -->
+
             @include('partials._topbar-dropdown')
-            <!-- End: Topbar-Dropdown -->
-
-            <!-- Start: Topbar -->
             @include('partials._topbar')
-            <!-- End: Topbar -->
 
-            <!-- Begin: Content -->
             <section id="content" class="table-layout animated fadeIn">
 
-            @yield('content')
+                @yield('content')
+                @include('partials._tray-right')
 
-            <!-- begin: .tray-right -->
-            @include('partials._tray-right')
-            <!-- end: .tray-right -->
             </section>
-            <!-- End: Content -->
 
-            <!-- Begin: Page Footer -->
             @include('partials._footer')
-            <!-- End: Page Footer -->
+
         </section>
-        <!-- End: Content-Wrapper -->
 
-        <!-- Start: Right Sidebar -->
         @include('partials._sidebar-right')
-        <!-- End: Right Sidebar -->
+
     </div>
-    <!-- End: Main -->
 
-    <!-- BEGIN: PAGE SCRIPTS -->
     @include('partials._javascript')
-
     @yield('scripts')
-    <!-- END: PAGE SCRIPTS -->
+
 </body>
 </html>
