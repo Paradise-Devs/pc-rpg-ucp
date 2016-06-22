40911a96aab96ede87a2d1093ee242df4f8f7eee
Lucas Godoy <lcsdegodoy@outlook.com>
2016-06-22 02:15:12 -0300

    Nova view de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,136 @@
-@extends('layouts.app')
-
+@extends('layouts.auth')
+@section('title', '- Login')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+<div class="admin-form theme-info" id="login1">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+  <div class="row mb15 table-layout">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="col-xs-6 va-m pln">
+      <a href="dashboard.html" title="Return to Dashboard">
+        <img src="assets/img/logos/logo_white.png" title="AdminDesigns Logo" class="img-responsive w250">
+      </a>
     </div>
+
+    <div class="col-xs-6 text-right va-b pr5">
+      <div class="login-links">
+        <a href="{{ url('/password/reset') }}" class="active" title="Esqueci minha senha">Esqueci minha senha</a>
+        <span class="text-white"> | </span>
+        <a href="pages_register.html" class="" title="Registrar">Registrar</a>
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="panel panel-info mt10 br-n">
+
+    <div class="panel-heading heading-border bg-white">
+      <span class="panel-title hidden">
+        <i class="fa fa-sign-in"></i>Registrar</span>
+      <div class="section row mn">
+        <div class="col-sm-4">
+          <a href="#" class="button btn-social facebook span-left mr5 btn-block" disabled>
+            <span>
+              <i class="fa fa-facebook"></i>
+            </span>Facebook</a>
+        </div>
+        <div class="col-sm-4">
+          <a href="#" class="button btn-social twitter span-left mr5 btn-block" disabled>
+            <span>
+              <i class="fa fa-twitter"></i>
+            </span>Twitter</a>
+        </div>
+        <div class="col-sm-4">
+          <a href="#" class="button btn-social googleplus span-left btn-block" disabled>
+            <span>
+              <i class="fa fa-google-plus"></i>
+            </span>Google+</a>
+        </div>
+      </div>
+    </div>
+
+    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+    {{ csrf_field() }}
+      <div class="panel-body bg-light p30">
+        <div class="row">
+          <div class="col-sm-7 pr30">
+
+            <div class="section row hidden">
+              <div class="col-md-4">
+                <a href="#" class="button btn-social facebook span-left mr5 btn-block">
+                  <span>
+                    <i class="fa fa-facebook"></i>
+                  </span>Facebook</a>
+              </div>
+              <div class="col-md-4">
+                <a href="#" class="button btn-social twitter span-left mr5 btn-block">
+                  <span>
+                    <i class="fa fa-twitter"></i>
+                  </span>Twitter</a>
+              </div>
+              <div class="col-md-4">
+                <a href="#" class="button btn-social googleplus span-left btn-block">
+                  <span>
+                    <i class="fa fa-google-plus"></i>
+                  </span>Google+</a>
+              </div>
+            </div>
+
+            <div class="section">
+              <label for="username" class="field-label text-muted fs18 mb10">E-mail</label>
+              <label for="email" class="field prepend-icon {{ $errors->has('email') ? ' has-error' : '' }}">
+                <input type="email" name="email" id="email" class="gui-input" value="{{ old('email') }}" placeholder="Entre com o email">
+                <label for="email" class="field-icon">
+                  <i class="fa fa-user"></i>
+                </label>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+              </label>
+            </div>
+
+            <div class="section">
+              <label for="username" class="field-label text-muted fs18 mb10">Senha</label>
+              <label for="password" class="field prepend-icon {{ $errors->has('password') ? ' has-error' : '' }}">
+                <input type="password" name="password" id="password" class="gui-input" placeholder="Entre com a senha">
+                <label for="password" class="field-icon">
+                  <i class="fa fa-lock"></i>
+                </label>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+              </label>
+            </div>
+
+          </div>
+          <div class="col-sm-5 br-l br-grey pl30">
+            <h3 class="mb25"> Paradise City RPG:</h3>
+            <p class="mb15">
+              <span class="fa fa-check text-success pr5"></span> Totalmente gratuido</p>
+            <p class="mb15">
+              <span class="fa fa-check text-success pr5"></span> Ilimitadas possibilidades</p>
+            <p class="mb15">
+              <span class="fa fa-check text-success pr5"></span> Conquiste propriedades</p>
+            <p class="mb15">
+              <span class="fa fa-check text-success pr5"></span> Conheça pessoas novas</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="panel-footer clearfix p10 ph15">
+        <button type="submit" class="button btn-primary mr10 pull-right">Acessar</button>
+        <label class="switch ib switch-primary pull-left input-align mt10">
+          <input type="checkbox" name="remember" id="remember" checked>
+          <label for="remember" data-on="SIM" data-off="NÃO"></label>
+          <span>Lembrar me</span>
+        </label>
+      </div>
+
+    </form>
+  </div>
 </div>
 @endsection
