b830c748bb57e4b514f3ca52d56ed1d92e8420c3
Lucas Godoy <lcsdegodoy@outlook.com>
2016-06-25 19:26:00 -0300

    Renomeação no nome da tabela reports

--- database/migrations/2016_06_25_185019_create_reports_table.php
+++ database/migrations/2016_06_25_185019_create_reports_table.php
@@ -12,7 +12,7 @@ class CreateReportsTable extends Migration
      */
     public function up()
     {
-        Schema::create('report', function (Blueprint $table) {
+        Schema::create('reports', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
         });
@@ -25,6 +25,6 @@ class CreateReportsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('report');
+        Schema::drop('reports');
     }
 }
