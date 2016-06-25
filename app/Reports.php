52961182af6f669b97e9419c044673f17d1deb3d
Lucas Godoy <lcsdegodoy@outlook.com>
2016-06-25 19:03:45 -0300

    Relação de denuncias e repostas feita

--- app/Reports.php
+++ app/Reports.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reports extends Model
 {
-    //
+    /**
+     * Get the answers of the report.
+     */
+    public function answers()
+    {
+        return $this->hasMany('App\ReportsAnswers');
+    }
 }
--- app/ReportsAnswers.php
+++ app/ReportsAnswers.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReportsAnswers extends Model
 {
-    //
+    /**
+     * Get the report that owns the answers.
+     */
+    public function post()
+    {
+        return $this->belongsTo('App\Reports');
+    }
 }
