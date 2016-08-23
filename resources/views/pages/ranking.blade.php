1e9a08bfde4eddcc24b3bcacc2617807707e0df2
Lucas Godoy <lcsdegodoy@outlook.com>
2016-08-23 14:58:16 -0300

    Remover tabelas de ranking não utilizadas

--- resources/views/pages/ranking.blade.php
+++ resources/views/pages/ranking.blade.php
@@ -127,7 +127,7 @@
             </div>
         </div>
     </div>
-    <div class="row">
+    {{--<div class="row">
         <div class="col-md-4">
             <div class="panel">
                 <div class="panel-heading">
@@ -194,7 +194,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>--}}
 @endsection
 <!--                                                                        -->
 @section('scripts')
