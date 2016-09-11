3815bdd8ccdce319f2061616b8ff872adc5773ce
Lucas Godoy <lcsdegodoy@outlook.com>
2016-09-11 15:55:14 -0300

    Alterar author e descrição do site

--- resources/views/partials/auth/_head.blade.php
+++ resources/views/partials/auth/_head.blade.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8">
 <title>PC-RPG @yield('title')</title>
 <meta name="keywords" content="RPG Paradise City SAMP SA:MP GTA San Andreas" />
-<meta name="description" content="Paradise City RPG">
-<meta name="author" content="Paradise Devs">
+<meta name="description" content="Paradise City RPG - SA:MP Server">
+<meta name="author" content="Paradise">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="theme-color" content="‪#009dc7">
 <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700'>
