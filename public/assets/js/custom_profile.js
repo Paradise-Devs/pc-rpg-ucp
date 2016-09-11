d510dd301895e883841ad2ee7be82cc847b87e07
Lós <los799@live.com>
2016-09-11 00:56:16 -0300

    Alterações de assets

--- public/assets/js/custom_profile.js
+++ public/assets/js/custom_profile.js
@@ -2,15 +2,14 @@
  * custom faq js for hiding elements while searching
  * ========================================================== */
 $(document).ready(function(){
-    $("#accconfig_btn").hide(1000);
-    $("#unfriend_btn").hide(1000);
-    $("#unblock_btn").hide(1000);
-    $("#blocked_info").hide(1000);
-
-    $('#friend_search').hideseek({
-        list:             '.friend_item',
-        ignore:           '.badge'
-    });
+    $("#acconfig_btn").hide();
+    $("#unfriend_btn").hide();
+    $("#unblock_btn").hide();
+    $("#changecover_btn").hide();
+    $("#removecover_btn").hide();
+    $("#report_btn_single").hide();
+    $("#blocked_info").hide();
+    $("#banned_info").hide();
 
     $('#player_select').multiselect({
         includeSelectAllOption: true,
@@ -19,8 +18,13 @@ $(document).ready(function(){
 
     $("#player_select").on('change', function() {
 		if($(this).val() == 'guest') {
-			$("#accconfig_btn").hide(1000);
+			$("#acconfig_btn").hide(1000);
             $("#unfriend_btn").hide(1000);
+            $("#changecover_btn").hide(1000);
+            $("#removecover_btn").hide(1000);
+            $("#banned_info").hide(1000);
+            $("#blocked_info").hide(1000);
+            $("#report_btn_single").hide(1000);
 
             $("#friend_btn").show(1000);
             $("#message_btn").show(1000);
@@ -28,8 +32,13 @@ $(document).ready(function(){
             $("#report_btn").show(1000);
         }
 		else if($(this).val() == 'friend') {
-            $("#accconfig_btn").hide(1000);
+            $("#acconfig_btn").hide(1000);
             $("#friend_btn").hide(1000);
+            $("#changecover_btn").hide(1000);
+            $("#removecover_btn").hide(1000);
+            $("#banned_info").hide(1000);
+            $("#blocked_info").hide(1000);
+            $("#report_btn_single").hide(1000);
 
             $("#unfriend_btn").show(1000);
             $("#message_btn").show(1000);
@@ -43,18 +52,43 @@ $(document).ready(function(){
             $("#message_btn").hide(1000);
             $("#report_btn").hide(1000);
             $("#unblock_btn").hide(1000);
+            $("#banned_info").hide(1000);
+            $("#blocked_info").hide(1000);
+            $("#report_btn_single").hide(1000);
 
-            $("#accconfig_btn").show(1000);
+            $("#acconfig_btn").show(1000);
+            $("#changecover_btn").show(1000);
+            $("#removecover_btn").show(1000);
         }
         else if($(this).val() == 'blocked') {
             $("#friend_btn").hide(1000);
             $("#unfriend_btn").hide(1000);
             $("#block_btn").hide(1000);
-            $("#message_btn").hide(1000);
             $("#report_btn").hide(1000);
+            $("#changecover_btn").hide(1000);
+            $("#removecover_btn").hide(1000);
+            $("#acconfig_btn").hide(1000);
+            $("#banned_info").hide(1000);
+            $("#blocked_info").hide(1000);
 
             $("#unblock_btn").show(1000);
+            $("#report_btn_single").show(1000);
             $("#blocked_info").show(1000);
+        }
+        else if($(this).val() == 'banned') {
+            $("#acconfig_btn").hide(1000);
+            $("#friend_btn").hide(1000);
+            $("#unfriend_btn").hide(1000);
+            $("#block_btn").hide(1000);
+            $("#message_btn").hide(1000);
+            $("#report_btn").hide(1000);
+            $("#unblock_btn").hide(1000);
+            $("#changecover_btn").hide(1000);
+            $("#removecover_btn").hide(1000);
+            $("#report_btn_single").hide(1000);
+            $("#blocked_info").hide(1000);
+
+            $("#banned_info").show(1000);
         }
     });
  });
