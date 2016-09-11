b2ce18b0e4327f8b1e3a08fd02c0aee28a4ce4c0
Lucas Godoy <lcsdegodoy@outlook.com>
2016-09-11 10:22:43 -0300

    Corrigir lista de jogadores online da dashboard, mostrava jogadores offline

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,8 +29,8 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $staff_online = User::select('users.id', 'username')->join('players', 'users.id', '=', 'players.user_id')->where('admin', '>', 1)->count();
-        $players      = User::select('users.id', 'users.id', 'username', 'admin', 'avatar_url')->join('players', 'users.id', '=', 'players.user_id')->get();
+        $staff_online = User::select('users.id', 'username')->join('players', 'users.id', '=', 'players.user_id')->where('admin', '>', 1)->where('players.isOnline', 1)->count();
+        $players      = User::select('users.id', 'users.id', 'username', 'admin', 'avatar_url')->join('players', 'users.id', '=', 'players.user_id')->where('players.isOnline', 1)->get();
         $ticket_count = Ticket::where('user_id', $user->id)->count();
         $report_count = Report::where('user_id', $user->id)->count();
         return view('pages.dashboard', ['staff_online' => $staff_online, 'players' => $players, 'tickets' => $ticket_count, 'reports' => $report_count]);
