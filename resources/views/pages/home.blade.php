447f7fa6335fa62179343c58dc2430b106fa54e2
Lucas Godoy <lcsdegodoy@outlook.com>
2016-09-18 20:58:17 -0300

    Remover v2 de link do css

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,11 +12,11 @@
 
         <!-- Stylesheets -->
         <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700'>
-        <link rel="stylesheet" type="text/css" href="assets/skin/default_skin/css/theme.css?v=2">
-        <link rel="stylesheet" type="text/css" href="assets/admin-tools/admin-forms/css/admin-forms.css?v=2">
+        <link rel="stylesheet" type="text/css" href="assets/skin/default_skin/css/theme.css">
+        <link rel="stylesheet" type="text/css" href="assets/admin-tools/admin-forms/css/admin-forms.css">
 
         <!-- Custom CSS -->
-        <link rel="stylesheet" type="text/css" href="assets/skin/default_skin/css/custom.css?v=2">
+        <link rel="stylesheet" type="text/css" href="assets/skin/default_skin/css/custom.css">
 
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="favicon.png">
