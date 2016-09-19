2eee26f9c792a787613498a662bf2e0d08623c38
Lucas Godoy <lcsdegodoy@outlook.com>
2016-09-18 22:31:01 -0300

    Adicionar construtor aos controllers faltantes

--- app/Http/Controllers/BugController.php
+++ app/Http/Controllers/BugController.php
@@ -23,6 +23,16 @@ use Gate;
 class BugController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
